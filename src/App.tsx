3c3d2842824e2e537ae3fcf3f6d30d3516b4f128
swallowed_o0 <musamea99@gmail.com>
2024-11-30 23:59:31 +0900

    🎉 Basic Clone Project

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async() => {
+    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {loading? <h1>Loading...</h1> : <div>{movies.map((movie) => 
+        <div key={movie.id}>
+          <img src={movie.medium_cover_image} alt="coverImg" />
+          <h2 className="text-3xl">{movie.title}</h2>
+          <p>{movie.summary}</p>
+          <ul>
+            {movie.genres.map((g) => {
+              <li key={g}>{g}</li>
+            })}
+          </ul>
+        </div>)}
+        </div>}
+    </div>
+  )
+}
