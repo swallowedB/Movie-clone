a5d817c0d90a2b026aa389acaf3c9313a49c800d
swallowed_o0 <musamea99@gmail.com>
2024-12-04 19:55:16 +0900

    Fix: update package.json and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import Detail from "./routes/Detail";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL} >
       <Routes>
-        <Route path={process.env.PUBLIC_URL + "/"}  element={<Home/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/movie/:id" element={<Detail/>} />
       </Routes>
     </BrowserRouter>
